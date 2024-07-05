cf25ceddc900ce441bfb8f93a189fba8488e3642
Wang Xin <xin_wang_xin@hotmail.com>
2024-07-05 14:23:12 +0800

    code for eb infra and app

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce6-tfstate"
+    key    = "wang-eb.tfstate" #Remember to change this
+    region = "ap-southeast-1"      #The region of your backend bucket
+  }
+}
+
